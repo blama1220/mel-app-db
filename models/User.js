1bad46b1410caf66f4903ab8111b29a44614bcc8
Brian López <brianlama1220@gmail.com>
2022-10-22 19:13:34 -0400

    update

---
+++ models/User.js
@@ -0,0 +1,18 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, lowercase: true },
+    work: String,
+    location: String,
+    age: Number,
+    gender: { type: String, enum: ["Male", "Female", ""] },
+    list: [{ moviesId: String, stateid: String }],
+  },
+  { timestamps: true }
+);
+
+const User = model("User", userSchema);
+export default User;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,27 +1,25 @@
 // const userControllers = require("../controllers/userController");
 const db = require("../models");
 module.exports = (app) => {
+  // API routes using userControllers
 
-    // API routes using userControllers
+  app.get("/movie", (req, res) => {
+    db.Entertainment.find({})
+      .limit(10)
+      .then((data) => {
+        res.json(data);
+      });
+  });
 
-    app.get("/movie", (req, res) => {
-        db.Entertainment.find({}).then((data)=> {
-            res.json(data);
-        });
+  app.post("/movie", (req, res) => {
+    db.Entertainment.create(req.body).then((data) => {
+      res.json(data);
     });
+  });
 
-    app.post("/movie", (req, res) => {
-        db.Entertainment.create(req.body).then((data)=> {
-            res.json(data);
-        });
+  app.get("/deleteAll", (req, res) => {
+    db.Entertainment.deleteMany({}).then((data) => {
+      res.json(data);
     });
-
-    app.get("/deleteAll", (req, res) => {
-        db.Entertainment.deleteMany({}).then((data)=> {
-            res.json(data);
-        });
-    });
-
-
-    
-}
+  });
+};
